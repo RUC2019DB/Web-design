2181450795ce35b4998bfa46d9ff0a2bb974199c
“WH“ <13522297261@139.com>
2021-12-09 13:13:24 +0800

    更改了格式

--- server.py
+++ server.py
@@ -314,7 +314,7 @@ def pay():
 
 
 if __name__ == '__main__':
-    db = dbQuery(dbIP='127.0.0.1',dbusername='sa',dbpassword='123456',dbname='Chinese')#数据库
+    db = dbQuery(dbIP='10.46.250.137',dbusername='sa',dbpassword='123456',dbname='Chinese')#数据库
     if db.ifconn:
         print("数据库连接成功")
         app.run(host=serverIP,port='5000')
